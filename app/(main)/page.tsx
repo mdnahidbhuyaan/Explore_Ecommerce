bc70530dc8003b6901ef14c8aafc565ee5ebcf01
mdnahidbhuyaan <mdnahidbuiyan14@gmail.com>
2025-07-29 10:44:37 +0600

    Add authentication page and UI components; update styles and dependencies

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -3,6 +3,7 @@ export default function Home() {
   return (
   <div>
     <h1  className='text-3xl flex items-center justify-center font-bold text-indigo-500'>Hello HomePage</h1>
+    <h2 className='text-3xl flex items-center justify-center font-bold text-indigo-500'>ddd</h2>
   </div>
   );
 }
